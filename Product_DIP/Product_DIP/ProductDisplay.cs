3db886029ccd30ae68ebb3d96f0f7b3aecada9cd
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-10-04 20:27:42 +0330

    Introduce ProductDisplay class with DIP compliance
    The `ProductDisplay` class was added to adhere to the Dependency Inversion Principle (DIP). It depends on the `IProductRepository` abstraction, enabling flexibility and decoupling from specific repository implementations.
    
    The `ShowProduct` method in `ProductDisplay` retrieves and displays products from the repository. Dependency injection is used to pass the repository instance.
    
    The `Main` method in `Program.cs` demonstrates the use of `ProductDisplay` with a `DatabaseProductRepository`. Commented-out lines suggest alternative repository implementations, highlighting the modularity of the design.
    
    These changes improve code modularity, testability, and adherence to SOLID principles.

---
+++ Product_DIP/Product_DIP/ProductDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Product_DIP
+{
+    class ProductDisplay
+    {
+        private IProductRepository _repository;
+
+        public ProductDisplay(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void ShowProduct()
+        {
+            var Products = _repository.GetProducts();
+            foreach(var item in Products)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Product_DIP/Product_DIP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_DIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductRepository repository = new DatabaseProductRepository();
+            //IProductRepository repository1 = new APIProductRepository();
+            //IProductRepository repository1 = new FileProductRepository();
+
+            ProductDisplay display = new ProductDisplay(repository);
+            display.ShowProduct();
+        }
+    }
+}
