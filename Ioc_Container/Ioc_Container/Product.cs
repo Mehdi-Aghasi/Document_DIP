270c1678f3d34a1f634ac372d8ef96905b1a2372
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-10-04 20:57:36 +0330

    add product

---
+++ Ioc_Container/Ioc_Container/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace Ioc_Container
+{
+    class Product
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"Id:{Id}\tName:{Name}\tPrice:{Price}");
+        }
+    }
+}
