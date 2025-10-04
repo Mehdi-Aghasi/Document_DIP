a94bf3929ae6cd8e14a71d83cf51c2bd38d4d491
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-10-04 20:41:05 +0330

    Add ProductDisplay and Program classes with DIP and Factory
    Introduced a `ProductDisplay` class that adheres to the Dependency
    Inversion Principle (DIP) by depending on the `IProductRepository`
    abstraction for displaying product data. Implemented a constructor
    to inject the repository dependency and a `ShowProduct` method to
    display product details.
    
    Added a `Program` class as the application entry point. Prompted
    the user to specify the data source type and used a
    `ProductRepositoryFactory` to create the appropriate repository
    implementation, following the Factory design pattern. Integrated
    error handling for invalid data source types using a custom
    `InvalidDataSourceException`.

---
+++ DIP_Factory/DIP_Factory/ProductDisplay.cs
@@ -0,0 +1,25 @@
+﻿using DIP_Factory;
+using System;
+using System.Collections.Generic;
+
+namespace DIP_Factory
+{
+    class ProductDisplay
+    {
+        private IProductRepository _repository;
+
+        public ProductDisplay(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void ShowProduct()
+        {
+            var Products = _repository.GetProducts();
+            foreach (var item in Products)
+            {
+                Console.WriteLine(item.Id + "\t" + item.Name + "\t" + item.Price);
+            }
+        }
+    }
+}
---
+++ DIP_Factory/DIP_Factory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the data source type[database/api/file]");
+            string input=Console.ReadLine();
+
+            try
+            {
+                IProductRepository repository = ProductRepositoryFactory.CreateRepository(input);
+                ProductDisplay display = new ProductDisplay(repository);
+                display.ShowProduct();
+            }
+            catch (InvalidDataSourceException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+    }
+}
